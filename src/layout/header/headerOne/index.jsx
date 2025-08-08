76085f3a12090337996b914fb6af4e173e248f28
Diego <diegob.development@gmail.com>
2025-08-08 12:46:31 -0500

    feat: remove unused headers

--- src/layout/header/headerOne/index.jsx
+++ src/layout/header/headerOne/index.jsx
@@ -1,35 +1,44 @@
-"use client";
-import ThemeOptionContext from "@/context/themeOptionsContext";
-import { useHeaderScroll } from "@/utils/hooks/HeaderScroll";
-import Cookies from "js-cookie";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useContext } from "react";
-import { RiHeartLine, RiMenuLine, RiUserLine } from "react-icons/ri";
-import { Button, Col, Container, Row } from "reactstrap";
-import HeaderCart from "../widgets/headerCart";
-import HeaderLogo from "../widgets/HeaderLogo";
-import HeaderSearchbar from "../widgets/headerSearchbar";
-import MainHeaderMenu from "../widgets/mainHeaderMenu";
-import TopBar from "../widgets/TopBar";
-import { useTranslation } from "react-i18next";
-import { Href } from "@/utils/constants";
+'use client'
+import ThemeOptionContext from '@/context/themeOptionsContext'
+import { useHeaderScroll } from '@/utils/hooks/HeaderScroll'
+import Cookies from 'js-cookie'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useContext } from 'react'
+import { RiHeartLine, RiMenuLine, RiUserLine } from 'react-icons/ri'
+import { Button, Col, Container, Row } from 'reactstrap'
+import HeaderCart from '../widgets/headerCart'
+import HeaderLogo from '../widgets/HeaderLogo'
+import HeaderSearchbar from '../widgets/headerSearchbar'
+import MainHeaderMenu from '../widgets/mainHeaderMenu'
+import TopBar from '../widgets/TopBar'
+import { useTranslation } from 'react-i18next'
+import { Href } from '@/utils/constants'
 
 const HeaderOne = () => {
-  const { themeOption, setOpenAuthModal, openAuthModal, mobileSideBar, setMobileSideBar } = useContext(ThemeOptionContext);
-  const UpScroll = useHeaderScroll(false);
-  const { t } = useTranslation("common");
-  const router = useRouter();
-  const isAuthenticated = Cookies.get("uat");
+  const {
+    themeOption,
+    setOpenAuthModal,
+    openAuthModal,
+    mobileSideBar,
+    setMobileSideBar,
+  } = useContext(ThemeOptionContext)
+  const UpScroll = useHeaderScroll(false)
+  const { t } = useTranslation('common')
+  const router = useRouter()
+  const isAuthenticated = Cookies.get('uat') && false
+  console.log(isAuthenticated, 'HERE AUTH')
   const handleProfileClick = (path) => {
-    isAuthenticated ? router.push("/account/dashboard") : setOpenAuthModal(true);
-  };
+    isAuthenticated ? router.push('/account/dashboard') : setOpenAuthModal(true)
+  }
   const handleWishlistClick = () => {
-    isAuthenticated ? router.push("/wishlist") : setOpenAuthModal(true);
-  };
+    isAuthenticated ? router.push('/wishlist') : setOpenAuthModal(true)
+  }
 
   return (
-    <header className={`${themeOption?.header?.sticky_header_enable && UpScroll ? "sticky fixed" : ""}`}>
+    <header
+      className={`${themeOption?.header?.sticky_header_enable && UpScroll ? 'sticky fixed' : ''}`}
+    >
       {themeOption?.header?.page_top_bar_enable && <TopBar />}
       <div className="metro">
         <Container>
@@ -37,22 +46,34 @@ const HeaderOne = () => {
             <Col sm="12">
               <div className="main-menu">
                 <div className="menu-left">
-                  <div className="toggle-nav" onClick={() => setMobileSideBar(!mobileSideBar)}>
+                  <div
+                    className="toggle-nav"
+                    onClick={() => setMobileSideBar(!mobileSideBar)}
+                  >
                     <RiMenuLine className="sidebar-bar" />
                   </div>
                   <div className="brand-logo">
                     <HeaderLogo />
                   </div>
                 </div>
+
                 <div className="menu-right pull-right">
                   <div className="main-navbar">
                     <div id="mainnav">
                       <div className="header-nav-middle">
                         <div className="main-nav navbar navbar-expand-xl navbar-light navbar-sticky">
-                          <div className={`offcanvas offcanvas-collapse order-xl-2 ${mobileSideBar ? "show" : ""} `}>
+                          <div
+                            className={`offcanvas offcanvas-collapse order-xl-2 ${mobileSideBar ? 'show' : ''} `}
+                          >
                             <div className="offcanvas-header navbar-shadow">
-                              <h5>{t("Menu")}</h5>
-                              <Button close className="lead" id="toggle_menu_btn" type="button" onClick={() => setMobileSideBar(false)}>
+                              <h5>{t('Menu')}</h5>
+                              <Button
+                                close
+                                className="lead"
+                                id="toggle_menu_btn"
+                                type="button"
+                                onClick={() => setMobileSideBar(false)}
+                              >
                                 <div>
                                   <i className="ri-close-fill"></i>
                                 </div>
@@ -73,7 +94,10 @@ const HeaderOne = () => {
                           <HeaderSearchbar />
                         </li>
                         <li className="onhover-div">
-                          <Link href={isAuthenticated ? "/wishlist" : Href} onClick={handleWishlistClick}>
+                          <Link
+                            href={isAuthenticated ? '/wishlist' : Href}
+                            onClick={handleWishlistClick}
+                          >
                             <RiHeartLine />
                           </Link>
                         </li>
@@ -81,10 +105,13 @@ const HeaderOne = () => {
                           <HeaderCart />
                         </li>
                         <li className="onhover-div">
-                      <Link href={isAuthenticated ? "/account/dashboard" : Href} onClick={handleProfileClick}>
-                        <RiUserLine />
-                      </Link>
-                    </li>
+                          <Link
+                            href={isAuthenticated ? '/account/dashboard' : Href}
+                            onClick={handleProfileClick}
+                          >
+                            <RiUserLine />
+                          </Link>
+                        </li>
                       </ul>
                     </div>
                   </div>
@@ -95,7 +122,7 @@ const HeaderOne = () => {
         </Container>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default HeaderOne;
+export default HeaderOne
--- src/layout/header/index.jsx
+++ src/layout/header/index.jsx
@@ -1,138 +1,20 @@
-import SettingContext from '@/context/settingContext'
-import ThemeOptionContext from '@/context/themeOptionsContext'
 import request from '@/utils/axiosUtils'
 import useFetchQuery from '@/utils/hooks/useFetchQuery'
-import { useSearchParams } from 'next/navigation'
-import React, { useContext, useEffect, useState } from 'react'
-import HeaderEight from './headerEight'
-import HeaderFive from './headerFive'
-import HeaderFour from './headerFour'
+import React, { useEffect } from 'react'
 import HeaderOne from './headerOne'
-import HeaderSeven from './headerSeven'
-import HeaderSix from './headerSix'
-import HeaderThree from './headerThree'
-import HeaderTwo from './headerTwo'
 
 const Headers = () => {
-    const { themeOption } = useContext(ThemeOptionContext)
-    const { setMenuLoader } = useContext(SettingContext)
-    const [style, setStyle] = useState('')
-    const path = useSearchParams()
-    const theme = path.get('theme')
+  const { refetch } = useFetchQuery(['menu'], () => request({ url: '/menu' }), {
+    select: (res) => res.data.data,
+    refetchOnWindowFocus: true,
+    enabled: false,
+  })
 
-    const headerOptions = {
-        header_one: <HeaderOne />,
-        header_two: <HeaderTwo />,
-        header_three: <HeaderThree />,
-        header_four: <HeaderFour />,
-        header_five: <HeaderFive />,
-        header_six: <HeaderSix />,
-        header_seven: <HeaderSeven />,
-        header_eight: <HeaderEight />,
-    }
-    useEffect(() => {
-        if (theme) {
-            if (
-                theme == 'fashion_one' ||
-                theme == 'gym' ||
-                theme == 'fashion_seven' ||
-                theme == 'fashion_two' ||
-                theme == 'surfboard' ||
-                theme == 'flower' ||
-                theme == 'yoga' ||
-                theme == 'fashion_three' ||
-                theme == 'fashion_four' ||
-                theme == 'electronics_two' ||
-                theme == 'jewellery_three' ||
-                theme == 'bag' ||
-                theme == 'watch' ||
-                theme == 'kids' ||
-                theme == 'beauty' ||
-                theme == 'goggles' ||
-                theme == 'video_slider' ||
-                theme == 'gradient' ||
-                theme == 'left_sidebar' ||
-                theme == 'parallax' ||
-                theme == 'vegetables_three' ||
-                theme == 'fashion_six' ||
-                theme == 'jewellery_two' ||
-                theme == 'medical' ||
-                theme == 'perfume' ||
-                theme == 'electronics_one' ||
-                theme == 'marketplace_one' ||
-                theme == 'tools' ||
-                theme == 'game' ||
-                theme == 'nursery'
-            ) {
-                setStyle('header_one')
-            } else if (theme == 'fashion_oned') {
-                setStyle('header_three')
-            } else if (
-                theme == 'fashion_ones' ||
-                theme == 'jewellery_one' ||
-                theme == 'christmas' ||
-                theme == 'digital_download' ||
-                theme == 'single_product'
-            ) {
-                setStyle('header_four')
-            } else if (
-                theme == 'fashion_ones' ||
-                theme == 'shoes' ||
-                theme == 'vegetables_one' ||
-                theme == 'marijuana'
-            ) {
-                setStyle('header_five')
-            } else if (
-                theme == 'fashion_ones' ||
-                theme == 'vegetables_two' ||
-                theme == 'furniture_two' ||
-                theme == 'electronics_three' ||
-                theme == 'books' ||
-                theme == 'pets' ||
-                theme == 'marketplace_two' ||
-                theme == 'marketplace_three'
-            ) {
-                setStyle('header_six')
-            } else if (theme == 'vegetables_four') {
-                setStyle('header_two')
-            } else if (theme == 'bicycle') {
-                setStyle('header_seven')
-            } else if (theme == 'fashion_ones' || theme == 'full_page') {
-                setStyle('header_eight')
-            }
-        } else {
-            let defaultStyle = themeOption?.header
-                ? themeOption?.header?.header_options
-                : 'header_one'
-            setStyle(defaultStyle)
-        }
-    }, [theme, style, themeOption, path])
+  useEffect(() => {
+    refetch()
+  }, [])
 
-    const {
-        data: headerMenu,
-        refetch,
-        isLoading,
-    } = useFetchQuery(['menu'], () => request({ url: '/menu' }), {
-        select: (res) => {
-            const originalData = res.data.data
-            const modifiedData = originalData.map((item) => ({
-                ...item,
-                class: `${['Product', 'Mega Menu'].includes(item.title) ? 1 : 0}`,
-            }))
-
-            return modifiedData
-        },
-        refetchOnWindowFocus: true,
-        enabled: false,
-    })
-
-    useEffect(() => {
-        refetch()
-    }, [])
-
-    useEffect(() => {}, [style])
-
-    return headerOptions[style]
+  return <HeaderOne />
 }
 
 export default Headers
