7f4018198d9de7fd599a5ecb5e982912e000d35f
Oscar Ivan Ramire <oscivaramtin@gmail.com>
2026-05-04 11:08:08 -0500

    * Send question about a product

--- src/components/pages/account/dashboard/ProfileInformation.jsx
+++ src/components/pages/account/dashboard/ProfileInformation.jsx
@@ -27,11 +27,11 @@ const ProfileInformation = () => {
                   {t("Phone")} : +{accountData?.country_code} {accountData?.phone}
                 </h6>
               </li>
-              {accountData?.address[0] ? (
+              {accountData?.address?.[0] ? (
                 <li>
                   <h6>
-                    {t("Address")} : {accountData?.address[0]?.street}
-                    {accountData?.address[0]?.city}, {accountData?.address[0]?.state.name}, {accountData?.address[0]?.country.name} {accountData?.address[0]?.pincode}
+                    {t("Address")} : {accountData?.address?.[0]?.street}
+                    {accountData?.address?.[0]?.city}, {accountData?.address?.[0]?.state?.name}, {accountData?.address?.[0]?.country?.name} {accountData?.address?.[0]?.pincode}
                   </h6>
                 </li>
               ) : null}
